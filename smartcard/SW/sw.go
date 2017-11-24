389e6167a5e76764e9562a09f34ea96eefc82337
Sebastian Fleissner <11110111000b@gmail.com>
2017-11-24 14:48:11 +1100

    Added package for common status words.

---
+++ smartcard/SW/sw.go
@@ -0,0 +1,13 @@
+package SW
+
+const (
+    AUTH_FAILED         uint16 = 0x63c0
+    WRONG_LENGTH        uint16 = 0x6700
+    RECORD_NOT_FOUND    uint16 = 0x6a83
+    UNSUPPORTED_INS     uint16 = 0x6d00
+    UNSUPPORTED_CLA     uint16 = 0x6e00
+    EXCEPTION           uint16 = 0x6f00
+    SUCCESS             uint16 = 0x9000
+    NOT_AUTHORIZED      uint16 = 0x91ae
+    INSUFFICIENT_MEMORY uint16 = 0x9210
+)
